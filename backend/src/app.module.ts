74afd33d6991a062f1ac6b19855506d054fa7960
musk96 <musk96@hotmail.fr>
2024-08-25 23:24:50 +0200

    added auth module in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { BlogModule } from './modules/blog/blog.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    BlogModule,
+    RouterModule.register([
+      {
+        path: 'api/v1/',
+        children: [
+          {
+            path: 'auth',
+            module: AuthModule,
+          },
+        ],
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/modules/auth/dto/auth-login.dto.ts
@@ -0,0 +1,8 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class AuthDto {
+  @IsNotEmpty()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+}
