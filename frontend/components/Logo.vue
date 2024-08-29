33fa9e0ae073de674c29db4845ca4bac018ab7b1
musk96 <musk96@hotmail.fr>
2024-08-29 13:31:03 +0200

    fixed responsivity

--- frontend/components/Logo.vue
+++ frontend/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
     <img src="https://cdn.worldvectorlogo.com/logos/webdev-1.svg" class="h-8" alt="Flowbite Logo" />
-    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DevByKamel</span>
+    <span class="hidden md:inline self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DevByKamel</span>
   </router-link>
 </template>
