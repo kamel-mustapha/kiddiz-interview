6bffb9f6c69ec40bac4075176fa7ec56ff2d0b61
musk96 <musk96@hotmail.fr>
2024-08-31 22:46:26 +0200

    removed kids logic

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -6,10 +6,6 @@ const links = [
     name: "Crèches",
     link: "/creches",
   },
-  {
-    name: "Enfants",
-    link: "/kids",
-  },
 ];
 </script>
 
@@ -19,10 +15,8 @@ const links = [
       <router-link to="/"><img src="https://kidizz.com/wp-content/uploads/2024/02/logo_kidizz.svg" alt="" /></router-link>
       <div class="bg-[#0D2436] text-white rounded-full px-6 py-3 gap-8 flex font-semibold">
         <router-link v-for="link in links" :to="link.link" class="hover:text-primary duration-200">{{ link.name }}</router-link>
+        <span class="text-primary">{{ user?.username }}</span>
       </div>
-      <span
-        >Welcome <span class="text-primary">{{ user?.username }}</span></span
-      >
     </div>
   </nav>
 </template>
